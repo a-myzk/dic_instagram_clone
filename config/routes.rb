fc00709500f3fdf5fae043f6ead500c239fee304
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-21 12:41:28 +0900

    メイラー設定変更(再)

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :favorites, only: [:index, :create, :destroy]
+  mount LetterOpenerWeb::Engine, at: "/inbox"
 end
